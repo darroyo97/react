a66b9409f6180bbf3d33f7d2c8dafac32039c36e
Daniela <daniela.arroyo27@gmail.com>
2020-03-30 11:41:08 -0500

    class examples

---
+++ Mar30/notes/Corona.js
@@ -0,0 +1,20 @@
+function Corona() {
+    return (<div>
+        <div className="row">
+            <div className="col s2">
+                <div className="card hoverable small">
+                    <div className="card-image">
+                        <img src="http://lorempixel.com/400/400/nature/" />
+                    </div>
+                    <div className="card-content">
+                        <p>{corona}</p>
+                        <p>N95 Masks</p>
+                    </div>
+                    <div className="card-action">
+                        <a href="#">{isMarkup ? 9.99 : 59.99}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>)
+}
