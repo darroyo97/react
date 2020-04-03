ee02b2896f249627acf732dc87288f8e81f38ecd
Daniela <daniela.arroyo27@gmail.com>
2020-04-03 11:07:55 -0500

    before refactor

--- Apr03/todo/src/App.js
+++ Apr03/todo/src/App.js
@@ -1,21 +1,61 @@
-import React, { Component } from 'react';
-// import H1 from './components/H1'
-// import H2 from './components/H2'
-// import H3 from './components/H3'
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
 
   constructor(props) {
     super(props)
 
+    this.state = {
+      tasks: [],
+      input: ""
+    }
+  }
+
+
+  handleInput = (e) => {
+    this.setState({
+      inputTxt: e.target.value
+    })
+  }
+
+  addTaskItem = () => {
+    // get current value of our inputTXT
+    let input = this.state.inputTxt
+    //current array
+    let tasks = this.state.tasks
+    //push
+    tasks.push(input);
+    this.setState({
+      tasks: tasks,
+      inputTxt: ""
+    }, () => {
+      console.log(this.state.tasks)
+    })
+    // this.setState({}, () => {
+
+    // } )
   }
 
   render() {
 
+    // let {tasks, inputTxt} = this.state 
+    let taskList = this.state.tasks.map((tasks, index) => {
+      return <li key={index}>{tasks}</li>
+    })
+
     return (
-      <>
-        the app
-      </>
+      <div className="todo-form">
+        <br />
+        <input type="text" value={this.state.inputTxt} onChange={this.handleInput} />
+
+        <button onClick={this.addTaskItem}>Add Task</button>
+        <br />
+
+        <ul>
+          {taskList}
+        </ul>
+      </div>
     )
   }
 }
