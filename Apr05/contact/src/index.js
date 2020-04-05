a3c45573a57539f58117ee6c69d0114b66566342
Daniela <daniela.arroyo27@gmail.com>
2020-04-05 13:54:56 -0500

    added bootstrap

--- Apr05/contact/src/index.js
+++ Apr05/contact/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-
+import BaseLayout from './components/layout/BaseLayout'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
 
